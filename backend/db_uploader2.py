52f87f87db5f8500208f888723ccb76d6ef09906
nyh <dkzndk1100@naver.com>
2022-03-06 16:33:16 +0900

    [fix] db_ulopader2.py 오타 수정
    - what : db_ulopader2.py 오타 수정
    - why : 오타 수정

--- backend/db_uploader2.py
+++ backend/db_uploader2.py
@@ -8,12 +8,12 @@ dotenv.read_dotenv()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
-from apps.video.models import LearningVideo 
+from apps.video.models import LearningVideo
 
 # 문제: 이러면 테이블에 id가 1부터 시작안하고 계속 증가함. (아예 테이블을 버리고 하는 방법이 있어야할듯)
 LearningVideo.objects.all().delete()
 
-CSV_PATH_PRODUCTS= os.path.join(os.path.abspath((os.path.dirname(__file__))), 'learning_video.csv') 
+CSV_PATH_PRODUCTS= os.path.join(os.path.abspath((os.path.dirname(__file__))), 'learning_video.csv')
 
 with open(CSV_PATH_PRODUCTS, encoding='utf8') as in_file:
     data_reader = csv.DictReader(in_file)
@@ -25,4 +25,4 @@ with open(CSV_PATH_PRODUCTS, encoding='utf8') as in_file:
                 category=row['category'],
                 difficulty=row['difficulty']
                 )
-        video.save()sss
+        video.save()
